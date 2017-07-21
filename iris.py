108abc600d83d804a588479cb4c41a7ac8fe6671
rsathishr <sathishamcet@gmail.com>
2017-07-21 22:37:33 +0530

    Update iris.py

--- iris.py
+++ iris.py
@@ -14,15 +14,20 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 
 
+#Loading the iris dataset from the direstory
 iris = pd.read_csv("/home/sathish/Downloads/iris.csv")
 
+#Converting the data to train the model
 iris['Species'] = iris.Species.map({'setosa': 0, 'versicolor': 1, 'virginica': 2})
 
+#trsin,test split using the train_test_split function of test file size 40%
 train, test = tt(iris, test_size=0.4, random_state=5)
 
+#Target Variable
 train_target = train.Species
 test_target = test.Species
 
+#Droping the target variable from the training data
 train.drop(['Species'],axis=1,inplace=True)
 test.drop(['Species'],axis=1,inplace=True)
 
@@ -32,11 +37,12 @@ test.drop(['Species'],axis=1,inplace=True)
 
 from sklearn.linear_model import LogisticRegression
 
+#Logistic Regression
 logreg = LogisticRegression()
-
 logr = logreg.fit(train,train_target)
 logr_pred = logreg.predict(test)
 
+#Confusion Matrix and Accuracy Score
 confusion_matrix(test_target, logr_pred)
 accuracy_score(test_target,logr_pred)
 
@@ -54,8 +60,8 @@ d_pred = dtree.predict(test)
 print(test_target)
 print(d_pred)
 
+#Confusion Matrix and Accuracy Score
 confusion_matrix(test_target, d_pred)
-
 accuracy_score(test_target,d_pred)
 
 ##################
@@ -69,6 +75,7 @@ knn = KNeighborsClassifier(n_neighbors=12)
 knn.fit(train,train_target)
 knn_pred = knn.predict(test)
 
+#Confusion Matrix and Accuracy Score
 print(confusion_matrix(test_target,knn_pred))
 accuracy_score(test_target,knn_pred)
 
@@ -82,6 +89,7 @@ rf = RandomForestClassifier(n_estimators=500)
 rf.fit(train,train_target)
 rf_pred = rf.predict(test)
 
+#Confusion Matrix and Accuracy Score
 print(confusion_matrix(test_target,rf_pred))
 accuracy_score(test_target,rf_pred)
 
@@ -97,6 +105,7 @@ svm = svm.SVC()
 svm.fit(train,train_target)
 svm_pred = svm.predict(test)
 
+#Confusion Matrix and Accuracy Score
 print(confusion_matrix(test_target,svm_pred))
 accuracy_score(test_target,svm_pred)
 
